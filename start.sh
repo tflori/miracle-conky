9ae7a14b3c810f6aad22e5297a7d2460ef59ccd9
Thomas Flori <t.flori@sportradar.com>
2018-02-22 11:12:30 +0100

    restart conky when it crashed

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 cd $(dirname $0)
 
 # wait for the network and other apps to start
 [[ " $@ " =~ " --no-sleep " ]] || sleep 10;
 
-conky -c miracle-config.lua > /tmp/conky.log 2>&1
+while true; do
+    conky -c miracle-config.lua >> /tmp/conky.log 2>&1
+done
+
